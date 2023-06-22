7bb3c6905fc652bb27897367c4bf9ec6fbe3561b
nimishp7 <nimishp7@gmail.com>
2023-06-22 09:21:09 +0530

    second commit

--- app/views.py
+++ app/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
+from .models import Employee, Role,  Department
 
 # Create your views here.
 
 
 def index(request):
-    return render(request,'index.html')
+    emps = Employee.objects.all()
+    return render(request,'index.html',{"emps":emps})
 
 def all_emp(request):
-    return render(request, 'all_emp.html')
+    emps = Employee.objects.all()
+    context = {
+        'emps': emps
+    }
+    print(context)
+    return render(request, 'all_emp.html', context)
 
 def add_emp(request):
     return render(request, 'add_emp.html')
